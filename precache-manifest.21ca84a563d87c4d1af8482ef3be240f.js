7eca6db1aa93a9a159ebe5cd956f15a353b54deb
Indra Bagus <indra.bagus@gmail.com>
2019-10-17 10:03:12 +0900

    Add for the first time

---
+++ precache-manifest.21ca84a563d87c4d1af8482ef3be240f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d2aa4f356030d6ff999bddf2be50f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0b7f021dcfc0d9ec2cc",
+    "url": "/static/css/main.56c0721f.chunk.css"
+  },
+  {
+    "revision": "0d76613999c67ccf99d7",
+    "url": "/static/js/2.ae709a5a.chunk.js"
+  },
+  {
+    "revision": "f0b7f021dcfc0d9ec2cc",
+    "url": "/static/js/main.afa8bb28.chunk.js"
+  },
+  {
+    "revision": "ee47fa0a29a125ddd092",
+    "url": "/static/js/runtime-main.f09da2dd.js"
+  }
+]);
