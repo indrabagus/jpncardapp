509fbb3bd8e1595c0dffbdc064977b385bc9a052
Indra Bagus <indra.bagus@gmail.com>
2019-10-20 22:00:14 +0900

    new looks

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jpncardapp/precache-manifest.f3da3b9fc7b5557e01355ce7932cf428.js"
+  "/jpncardapp/precache-manifest.c32478fa64fab6aa9c109e98e64e2222.js"
 );
 
 self.addEventListener('message', (event) => {
