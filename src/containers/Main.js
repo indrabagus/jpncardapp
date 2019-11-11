80d577ee75ff5e23edaf89417b22cf3f56a782c6
Indra Bagus <indra.bagus@gmail.com>
2019-11-11 22:37:16 +0900

    Finishing Menu Toolbar

---
+++ src/containers/Main.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu'
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme)=>({
+  toolbar:{
+    marginTop : 10,
+    borderTop : `1px solid ${theme.palette.divider}`,
+    borderBottom:`1px solid ${theme.palette.divider}`
+  },
+  toolbarTitle: {
+    flex:1,
+    fontSize:24,
+  },
+}));
+
+export default function MainContainer(props) {
+  const classes = useStyles();
+  return(
+    <React.Fragment>
+    <Container maxWidth='lg'>
+      <Toolbar className={classes.toolbar}>
+        <IconButton>
+          <MenuIcon/>
+        </IconButton>
+      <Typography 
+        className={classes.toolbarTitle}
+        component="h2"
+        variant="h5"
+        color="inherit"
+        noWrap
+      >
+        Japan Card Application
+      </Typography>
+
+      </Toolbar>
+    </Container>
+    </React.Fragment>    
+  );
+};
+
