7e383570e8d158b990b63e0c30a49b29ae41218c
Indra Bagus <indra.bagus@gmail.com>
2019-11-16 12:56:25 +0900

    Signin container

---
+++ src/containers/SignIn.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Copyright from '../components/Copyright';
+import AuthContext from '../services/jcardContext';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn(){
+  const classes = useStyles();
+  const authctx = React.useContext(AuthContext);
+  function OnSignIn(props){
+    authctx.action({
+      type:"LOGIN",
+        payload:{
+        isLocal:true,
+        token:null
+      }
+    });
+  }
+
+  const FormLogin = (
+    <form className={classes.form} noValidate>
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        label="Username"
+        autoComplete="username"
+        autoFocus
+      />
+      <TextField 
+        variant="outlined"
+        margin="normal"
+        type="password"
+        label="Password"
+        autoComplete="current-password"
+        required
+        fullWidth
+      />
+      <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Remember me"
+      />
+      <Button
+        className={classes.submit}
+        type="button"
+        variant="contained"
+        color="primary"
+        fullWidth
+        onClick={OnSignIn}
+      >
+        SignIn
+      </Button>
+    </form>
+  );
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        { FormLogin }
+      <Grid container>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {"Contact Email: indra.bagus@gmail.com"}
+          </Link>
+        </Grid>
+      </Grid>
+        
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
