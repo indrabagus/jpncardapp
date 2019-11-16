2dec6fb883f93bcf6b79ff12616b42870ea03390
Indra Bagus <indra.bagus@gmail.com>
2019-11-16 21:33:58 +0900

    implementation of card and its integration

---
+++ src/components/Card.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme)=>({
+  card : {
+    maxWidth:400,
+    minWidth:290,
+    [theme.breakpoints.up('sm')]: {
+      maxWidth:600,      
+      minWidth:400,
+    },
+    
+    [theme.breakpoints.up('md')]: {
+      maxWidth:1024,      
+      minWidth:800,
+    },
+
+    borderRadius:10,
+  },
+  title: {
+    fontSize:'1rem',
+    [theme.breakpoints.up('md')]: {
+      fontSize:'2rem',
+    }    
+  },
+
+  vocabulary : {
+    fontSize:'1rem',
+    [theme.breakpoints.up('md')]: {
+      fontSize:'4rem',
+    }
+  },
+
+  pos: {
+    // fontSize:22,
+    marginBottom:12,
+  },
+}));
+
+export default function VoCard(props){
+  const classes = useStyles();
+  return(
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Vocabulary Card
+        </Typography>
+        <Typography className={classes.vocabulary} >
+          勉強する
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Irregular verbs
+        </Typography>
+        <Typography variant="body2" component="p">
+          to study (~を).
+          <br />
+          {'"図書館で勉強するができます。"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,21 +3,33 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
 import MenuIcon from '@material-ui/icons/Menu'
 import {makeStyles} from '@material-ui/core/styles';
 import AppDrawer from '../components/AppsDrawer';
-
+import VoCard from '../components/Card';
 
 const useStyles = makeStyles((theme)=>({
   toolbar:{
-    marginTop : theme.spacing(1),
+    marginTop : theme.spacing(2),
+    marginBottom:theme.spacing(5),
     borderTop : `1px solid ${theme.palette.divider}`,
     borderBottom:`1px solid ${theme.palette.divider}`,
   },
   toolbarTitle: {
     flex:1,
-    fontSize:24,
+    fontSize:'1rem',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '2rem',
+    },    
   },
+
+  card : {
+    marginTop : theme.spacing(1),
+    display:'flex',
+    alignItems:'centre',
+    justifyContent: 'flex-end',
+  }
 }));
 
 export default function MainContainer(props) {
@@ -35,21 +47,33 @@ export default function MainContainer(props) {
         <IconButton onClick={handleDrawerOpen}>
           <MenuIcon/>
         </IconButton>
-      <Typography 
-        className={classes.toolbarTitle}
-        component="h2"
-        variant="h5"
-        color="inherit"
-        noWrap
-      >
-        Japan Card Application
-      </Typography>
-
+        <Typography 
+          className={classes.toolbarTitle}
+          component="h2"
+          variant="h5"
+          color="inherit"
+          noWrap
+        >
+          Japan Card Application
+        </Typography>
       </Toolbar>
       <AppDrawer
         open={drawerOpen}
         onClose={handleDrawerOpen}
       />
+      <Grid  
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        // justify="center"
+        // style={{ minHeight: '100vh' }} 
+      >
+        <Grid item xs={12}>
+        <VoCard/>
+        </Grid>
+
+      </Grid>
     </Container>
     </React.Fragment>
   );
