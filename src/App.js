38f5468460983e01e36b28b9b79663f435719fd4
Indra Bagus <indra.bagus@gmail.com>
2019-11-22 11:04:10 +0900

    clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React,{useEffect} from 'react';
+import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
 import SwitchRoute from './services/jcardRouter';
 function App() {
-  // const [token, setToken] = useState();
-  useEffect(() => {
-  }, []);
 
   return (
     <Router>
