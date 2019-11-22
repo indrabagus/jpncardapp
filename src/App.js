0ab514c77591dddf3a60277b494aa2740f31c406
Indra Bagus <indra.bagus@gmail.com>
2019-11-22 12:01:45 +0900

    use basename

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
 import SwitchRoute from './services/jcardRouter';
+import {Config} from './services/jcConfig';
 function App() {
 
   return (
-    <Router>
+    <Router basename={Config.url.BASENAME}>
       <SwitchRoute/>
     </Router>
   );
