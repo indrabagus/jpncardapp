dbff8a397e739f86dc23bc94e9654ac340712667
Indra Bagus <indra.bagus@gmail.com>
2019-11-28 11:07:05 +0900

    Add circular progress

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -7,6 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import MenuIcon from '@material-ui/icons/Menu'
 import {makeStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import AppDrawer from '../components/AppsDrawer';
 import VoCard from '../components/Card';
@@ -38,7 +39,7 @@ const useStyles = makeStyles((theme)=>({
     fontSize:'1rem',
     [theme.breakpoints.up('sm')]: {
       fontSize: '2rem',
-    },    
+    },
   },
 
   card : {
@@ -56,6 +57,7 @@ export default function MainContainer(props) {
   const classes = useStyles();
   const [drawerOpen,setDrawerOpen]=React.useState(false);
   const [cardvalue,setCardValue] = React.useState({});
+  const [busy,setBusy] = React.useState(false);
   const appctx = React.useContext(AppContext);
   function handleDrawerOpen(){
     setDrawerOpen(!drawerOpen);
@@ -63,6 +65,7 @@ export default function MainContainer(props) {
 
   /* Get Card data from the backend */
   function onGetCardData(){
+    setBusy(true);
     getCardData(appctx.state.token,appctx.state.currUrl)
     .then((resp)=>{
       setCardValue(resp.result);
@@ -78,6 +81,7 @@ export default function MainContainer(props) {
         console.log("ERROR :",err);
       }
     });
+    setBusy(false);
   }
 
   React.useEffect(onGetCardData,[]);
@@ -96,7 +100,7 @@ export default function MainContainer(props) {
         alt=""
       />
     </Icon>
-    <Typography 
+    <Typography
       className={classes.toolbarTitle}
       component="h2"
       variant="h5"
@@ -116,20 +120,23 @@ export default function MainContainer(props) {
         onClose={handleDrawerOpen}
         onGetCard={onGetCardData}
       />
-      <Grid  
+      <Grid
         container
         spacing={0}
         direction="column"
         alignItems="center"
         // justify="center"
-        // style={{ minHeight: '100vh' }} 
+        // style={{ minHeight: '100vh' }}
       >
         <Grid item xs={12}>
-        <VoCard 
-          data={cardvalue}
-          title={appctx.state.cardTitle}
-          onGetCard={onGetCardData}
-        />
+          { 
+            busy ?  <CircularProgress/> : 
+                    <VoCard
+                      data={cardvalue}
+                      title={appctx.state.cardTitle}
+                      onGetCard={onGetCardData}
+                    />
+          }
         </Grid>
 
       </Grid>
