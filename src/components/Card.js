373024600ea0abd3657b85ec912f1890c1fc337e
Indra Bagus <indra.bagus@gmail.com>
2019-12-02 22:21:29 +0900

    fix viewing port and using circular progress

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,26 +5,16 @@ import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles((theme)=>({
   card : {
-    maxWidth:290,
-    minWidth:290,
-    [theme.breakpoints.up('375')]: {
-      maxWidth:340,
-      minWidth:340,
-    },
-
-    [theme.breakpoints.up('400')]: {
-      maxWidth:380,
-      minWidth:380,
+    maxWidth:'85vw',
+    minWidth:'85vw',
+    [theme.breakpoints.up('lg')]: {
+      maxWidth:'60vw',
+      minWidth:'60vw',
     },    
-    
-    [theme.breakpoints.up('md')]: {
-      maxWidth:1024,
-      minWidth:800,
-    },
-
     borderRadius:10,
   },
 
@@ -37,8 +27,16 @@ const useStyles = makeStyles((theme)=>({
 
   vocabulary : {
     fontSize:'1.5rem',
+    [theme.breakpoints.up('375')]: {
+      fontSize:'2.5rem',
+    },
+
+    [theme.breakpoints.up('400')]: {
+      fontSize:'2.6rem',
+    },    
+
     [theme.breakpoints.up('md')]: {
-      fontSize:'4rem',
+      fontSize:'2rem',
     }
   },
 
@@ -51,14 +49,37 @@ const useStyles = makeStyles((theme)=>({
   },
 }));
 
+export function VoErrorCard(props){
+  const classes = useStyles();
+  return(
+    <Card 
+      style={{ backgroundColor: '#ECEFF1',height: '30vh' }} 
+      className={classes.card}>
+      <CardContent onClick={props.onGetCard}>
+        <Typography style = {{color:'red'}} className={classes.pos}>
+          {props.errormsg}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
 export default function VoCard(props){
   const classes = useStyles();
   return(
-    <Card className={classes.card} >
-      <CardContent onClick={props.onGetCard}>
+    <div>
+    <Card 
+      style={{height:'40vh',backgroundColor: '#ECEFF1' }} 
+      className={classes.card} >
+      <CardContent 
+        style={{height:'27vh'}}
+        onClick={props.onGetCard}>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
           {props.title}
         </Typography>
+        {props.isBusy ? (
+          <CircularProgress style={{margin:20}}/>
+        ) : (
+          <React.Fragment>
         <Typography className={classes.vocabulary} >
           { props.data.kanji ? props.data.kanji:props.data.kana}
         </Typography>
@@ -66,14 +87,19 @@ export default function VoCard(props){
           {/* If it's not kanji write unicode 'space' (\u00a0) instead */}
           { props.data.kanji ? '[ '+props.data.kana+' ]':"\u00a0"} 
         </Typography>
-        <Typography variant="body2" component="p">
+        <Typography style={{fontSize:'1.1rem'}} variant="body2" component="p">
           "{props.data.english}"
-          {/* <br />
+            {/* TODO: Example Given 
+            <br />
           <br />
           e.g: {'"図書館で勉強するができます。"'} */}
         </Typography>
+
+          </React.Fragment>
+        ) 
+        }
       </CardContent>
-      <CardActions>
+      <CardActions style={{paddingBottom:'1px'}}>
         <Button 
           size="medium"
           onClick={props.onGetCard}
@@ -82,5 +108,6 @@ export default function VoCard(props){
         </Button>
       </CardActions>
     </Card>
+    </div>
   );
 }
